d70b5f911e97cf60d0fbd6b9d9039b9d203939a1
kamil <1484313@gmail.com>
2018-11-07 12:23:01 +0100

    create projects

--- Gemfile
+++ Gemfile
@@ -16,6 +16,13 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
+gem 'devise'
+gem 'milia'
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
+gem 'bootstrap-datepicker-rails'
+gem 'jquery-rails'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -62,3 +69,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'activerecord-session_store', github: 'rails/activerecord-session_store'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	resources :tenants do
+  	resources :projects
+  end
+  
+  resources :members
+  get 'home/index'
+
+   root :to => "home#index"
+
+    
+  # *MUST* come *BEFORE* devise's definitions (below)
+  as :user do   
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+  end
+
+  devise_for :users, :controllers => { 
+    :registrations => "milia/registrations",
+    :confirmations => "confirmations",
+    :sessions => "milia/sessions", 
+    :passwords => "milia/passwords", 
+  }
+
+
 end
